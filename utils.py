624b7830bf9585ade49e74b9ddb445007de5282e
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2024-11-22 22:22:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from gtts import gTTS
 import os
 from deep_translator import GoogleTranslator
 
-GEMINI_API_KEY='AIzaSyAPxwTCymUnkIAwqJhiaIwgVhvm0RwTHpQ'
+GEMINI_API_KEY='YOUR_GEMINI_API_KEY'
 gai.configure(api_key=GEMINI_API_KEY)
 model = gai.GenerativeModel('gemini-1.5-flash')
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -27,7 +27,7 @@ def translate_text(text, target_language):
     translation = GoogleTranslator(source='auto', target=target_language).translate(text)
     return translation
 
-def text_to_speech(text, lang='en'):
+def text_to_speech(text, lang):
     tts = gTTS(text=text, lang=lang)
     filename = "response.mp3"
     tts.save(filename)
